3ea708c3136cafd99ca164268ffde23059ee292a
Vivekbansal338 <Vivekb379@gmail.com>
2023-07-26 13:31:27 +0530

    backend change

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -23,22 +23,31 @@ exports.getSingleUser = async (req, res) => {
 
 //controller for creating a user
 exports.createUser = async (req, res) => {
+  const { email, password, username } = req.body;
+  const finalEmail = email.toLowerCase();
+  const finalUsername = username.toLowerCase();
+  const finalPassword = password;
+  const temp = {
+    email: finalEmail,
+    password: finalPassword,
+    username: finalUsername,
+  };
   try {
-    const user = await users.create(req.body);
+    const user = await users.create(temp);
     if (user) {
-      res.status(200).json(user);
+      res.status(200).json({ status: "success", user: user });
     } else {
-      res.json({ message: "User already exists" });
+      res.json({ status: "unsuccess", message: "User already exists" });
     }
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.json({ error: err });
   }
 };
 
 exports.loginUser = async (req, res) => {
   try {
     const user = await users.findOne({
-      email: req.body.email,
+      email: req.body.email.toLowerCase(),
       password: req.body.password,
     });
     if (user) {
